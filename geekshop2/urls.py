c7e158fa5e2305fcc4f391c56ff75d3a4507e0b5
Denis Volkov <denis-not@ya.ru>
2019-04-21 14:46:31 +0300

    В настроики добавлена работа с media

--- geekshop2/urls.py
+++ geekshop2/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import mainapp.views as mainapp
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = [
     path('', mainapp.main, name='main'),
